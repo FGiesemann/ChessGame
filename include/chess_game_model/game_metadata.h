ee72bb01ad72ed1be8d94bf23d817ff74b6d8e05
Florian Giesemann <fgiesemann@proton.me>
2025-07-27 11:31:46 +0200

    Setup project

---
+++ include/chess_game_model/game_metadata.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** *
+ * Chess Game Model                                                           *
+ * Representation of a single game of chess                                   *
+ * ************************************************************************** */
+/** \file */
+
+#ifndef CHESSGAMEMODEL_GAME_METADATA_H
+#define CHESSGAMEMODEL_GAME_METADATA_H
+
+namespace chessgamemodel {
+
+/**
+ * \brief Stores meta data for a game of chess.
+ *
+ * Handles meta data like the names of players, the event, etc.
+ */
+class GameMetadata {
+    // TODO: Store something...
+};
+
+} // namespace chessgamemodel
+
+#endif
---
+++ src/game.cpp
@@ -0,0 +1,8 @@
+/* ************************************************************************** *
+ * Chess Game Model                                                           *
+ * Representation of a single game of chess                                   *
+ * ************************************************************************** */
+
+#include "chess_game_model/game.h"
+
+namespace chesscore {}
