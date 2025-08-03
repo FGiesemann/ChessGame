e0155415e1eccee60271e3fa933b1c61f2773531
Florian Giesemann <florian.giesemann@gmail.com>
2025-08-03 15:00:00 +0200

    Prepare PGN lexer tests

---
+++ test/src/pgn_lexer_test.cpp
@@ -0,0 +1,12 @@
+/* ************************************************************************** *
+ * Chess Game                                                                 *
+ * Representation of a single game of chess                                   *
+ * ************************************************************************** */
+
+#include <catch2/catch_all.hpp>
+
+#include "chessgame/pgn.h"
+
+using namespace chessgame;
+
+TEST_CASE("PGN.Lexer", "[pgn]") {}
