f778a434e6ed1d10d81fb0d8593609c999815c0d
Florian Giesemann <florian.giesemann@gmail.com>
2025-08-07 19:18:02 +0200

    D Add pgn_database

--- pgn_parser/pgn_parser.cpp
+++ pgn_parser/pgn_parser.cpp
@@ -1,8 +1,17 @@
 #include "chessgame/pgn.h"
 
+#include "pgn_database.h"
+
 #include <iostream>
+#include <sstream>
 
 auto main() -> int {
-    std::cout << "Hello, World!\n";
+    try {
+        auto pgn_data = std::istringstream{pgn_database};
+        auto parser = chessgame::PGNParser{pgn_data};
+        parser.read_game();
+    } catch (chessgame::PGNError &e) {
+        std::cout << "PGNError:\n" << "  type: " << to_string(e.type()) << '\n' << "  line: " << e.line() << '\n' << "  message: " << e.what() << '\n';
+    }
     return 0;
 }
