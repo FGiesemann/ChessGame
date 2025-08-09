42296255021673aa9f1e7a8b477dc607818c477e
Florian Giesemann <fgiesemann@proton.me>
2025-08-09 12:50:30 +0200

    Extend PGN Parser test

--- test/src/pgn_parser_test.cpp
+++ test/src/pgn_parser_test.cpp
@@ -12,6 +12,17 @@
 
 using namespace chessgame;
 
+auto count_ply_on_mainline(const chessgame::Game &game) {
+    const auto cursor = game.cursor();
+    int child_count{0};
+    auto next_cursor = cursor.child(0);
+    while (next_cursor.has_value()) {
+        ++child_count;
+        next_cursor = next_cursor.value().child(0);
+    }
+    return child_count;
+}
+
 TEST_CASE("PGN.Parser.Simple Linear Game", "[pgn]") {
     const std::string game_data = R"([Event "Test Event"]
 [Site "Test Site"]
@@ -24,4 +35,5 @@ TEST_CASE("PGN.Parser.Simple Linear Game", "[pgn]") {
     auto parser = chessgame::PGNParser{pgn_data};
     auto game = parser.read_game();
     CHECK(game.has_value());
+    CHECK(count_ply_on_mainline(game.value()) == 7);
 }
