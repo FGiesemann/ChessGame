e6c32d1b557cb7b694840e36be7dd792bd6d60d6
Florian Giesemann <florian.giesemann@gmail.com>
2025-08-16 13:17:45 +0200

    Test PGN Parser with commented game

--- test/src/pgn_parser_test.cpp
+++ test/src/pgn_parser_test.cpp
@@ -5,8 +5,10 @@
 
 #include <catch2/catch_all.hpp>
 
+#include "chesscore_io/chesscore_io.h"
 #include "chessgame/pgn.h"
 
+#include <algorithm>
 #include <sstream>
 #include <string>
 
@@ -26,6 +28,12 @@ auto count_ply_on_mainline(const chessgame::Game &game) -> int {
 
 using GamePath = std::vector<int>;
 
+auto mainline(size_t depth) -> GamePath {
+    GamePath path(depth);
+    std::fill(path.begin(), path.end(), 0);
+    return path;
+}
+
 auto get_node(const chessgame::Game &game, const GamePath &path) -> std::shared_ptr<const chessgame::GameNode> {
     auto cursor = game.cursor();
     for (size_t index = 0U; index < path.size(); ++index) {
@@ -44,6 +52,7 @@ auto get_move(const chessgame::Game &game, const GamePath &path) -> Move {
 auto check_move(const chessgame::Game &game, const GamePath &path, const Move &expected_move) -> void {
     CAPTURE(path);
     const auto &move = get_move(game, path);
+    CAPTURE(move);
     CHECK(move == expected_move);
 }
 
@@ -66,12 +75,44 @@ TEST_CASE("PGN.Parser.Simple Linear Game", "[pgn]") {
     auto opt_game = parser.read_game();
     REQUIRE(opt_game.has_value());
     const auto &game = opt_game.value();
-    CHECK(count_ply_on_mainline(game) == 7);
 
-    check_move(game, {0}, Move{.from = Square::E2, .to = Square::E4, .piece = Piece::WhitePawn});
-    check_move(game, {0, 0}, Move{.from = Square::E7, .to = Square::E5, .piece = Piece::BlackPawn});
-    check_move(game, {0, 0, 0}, Move{.from = Square::G1, .to = Square::F3, .piece = Piece::WhiteKnight});
-    check_move(game, {0, 0, 0, 0}, Move{.from = Square::B8, .to = Square::C6, .piece = Piece::BlackKnight});
-    check_move(game, {0, 0, 0, 0, 0, 0, 0}, Move{.from = Square::B5, .to = Square::A4, .piece = Piece::WhiteBishop});
-    CHECK(has_no_following_move(game, {0, 0, 0, 0, 0, 0, 0}));
+    CHECK(count_ply_on_mainline(game) == 7);
+    check_move(game, mainline(1), Move{.from = Square::E2, .to = Square::E4, .piece = Piece::WhitePawn});
+    check_move(game, mainline(2), Move{.from = Square::E7, .to = Square::E5, .piece = Piece::BlackPawn});
+    check_move(game, mainline(3), Move{.from = Square::G1, .to = Square::F3, .piece = Piece::WhiteKnight});
+    check_move(game, mainline(4), Move{.from = Square::B8, .to = Square::C6, .piece = Piece::BlackKnight});
+    check_move(game, mainline(7), Move{.from = Square::B5, .to = Square::A4, .piece = Piece::WhiteBishop});
+    CHECK(has_no_following_move(game, mainline(7)));
+}
+
+TEST_CASE("PGN.Parser.Game with Comments", "[pgn]") {
+    const std::string game_data = R"([Event "Test Event"]
+[Site "Test Site"]
+[White "Player W"]
+[Black "Player B"]
+[Result "1-0"]
+
+1. d4 d5 2. c4 e6 3. Nc3 Nf6 4. Bg5 {It was Pillsbury who first demonstrated the
+strength of the this move, which today is routine} 4..Be7 5. Nf3 Nbd7 6. Rc1 O-O
+7. e3 b6 {In order to develop the Queen Bishop on Bb7. This was the most popular
+way of defending the Queen's gambit declined at the time.} 8. cxd5 {Depriving
+Black of the opportunity to play dxc4 when the diagonal b7-g2 would be open for
+his Queen Bishop.} 8...exd5 9. Bd3 Bb7 10. O-O c5 1-0)";
+
+    std::istringstream pgn_data{game_data};
+    auto parser = chessgame::PGNParser{pgn_data};
+    auto opt_game = parser.read_game();
+    REQUIRE(opt_game.has_value());
+    const auto &game = opt_game.value();
+
+    CHECK(count_ply_on_mainline(game) == 20);
+    check_move(game, mainline(1), Move{.from = Square::D2, .to = Square::D4, .piece = Piece::WhitePawn});
+    check_move(game, mainline(2), Move{.from = Square::D7, .to = Square::D5, .piece = Piece::BlackPawn});
+    check_move(game, mainline(7), Move{.from = Square::C1, .to = Square::G5, .piece = Piece::WhiteBishop});
+    check_move(game, mainline(8), Move{.from = Square::F8, .to = Square::E7, .piece = Piece::BlackBishop});
+    check_move(game, mainline(12), Move{.from = Square::E8, .to = Square::G8, .piece = Piece::BlackKing});
+    check_move(game, mainline(14), Move{.from = Square::B7, .to = Square::B6, .piece = Piece::BlackPawn});
+    check_move(game, mainline(15), Move{.from = Square::C4, .to = Square::D5, .piece = Piece::WhitePawn, .captured = Piece::BlackPawn});
+    check_move(game, mainline(16), Move{.from = Square::E6, .to = Square::D5, .piece = Piece::BlackPawn, .captured = Piece::WhitePawn});
+    check_move(game, mainline(20), Move{.from = Square::C7, .to = Square::C5, .piece = Piece::BlackPawn});
 }
