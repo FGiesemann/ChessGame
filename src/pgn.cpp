7802f58bf28b268152930115163f49786015a8de
Florian Giesemann <fgiesemann@proton.me>
2025-08-24 17:20:38 +0200

    Add promotion symbol to allowed symbols

--- src/pgn.cpp
+++ src/pgn.cpp
@@ -139,9 +139,9 @@ auto PGNLexer::read_token_starting_with_number(char first_c) -> Token {
 }
 
 auto PGNLexer::is_symbol_character(char character) -> bool {
-    static std::set<char> symbol_characters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                                               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                                               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '/', '+', '#'};
+    static std::set<char> symbol_characters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+                                               'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                                               'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '/', '+', '#', '='};
     return symbol_characters.contains(character);
 }
 
