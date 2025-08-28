42a1a2d946eb5880495923c0c0a18e4e264640b9
Florian Giesemann <fgiesemann@proton.me>
2025-08-28 18:10:11 +0200

    Make GameMetadata its own class

--- include/chessgame/metadata.h
+++ include/chessgame/metadata.h
@@ -7,6 +7,7 @@
 #ifndef CHESSGAME_GAME_METADATA_H
 #define CHESSGAME_GAME_METADATA_H
 
+#include <array>
 #include <optional>
 #include <string>
 #include <vector>
@@ -26,7 +27,59 @@ struct metadata_tag {
 /**
  * \brief A collection of metadata tags.
  */
-using GameMetadata = std::vector<metadata_tag>;
+class GameMetadata {
+public:
+    using iterator = std::vector<metadata_tag>::iterator;
+    using const_iterator = std::vector<metadata_tag>::const_iterator;
+
+    iterator begin() { return m_tags.begin(); }
+    iterator end() { return m_tags.end(); }
+    const_iterator begin() const { return m_tags.begin(); }
+    const_iterator end() const { return m_tags.end(); }
+
+    static const std::array<std::string, 7> str_tags;
+
+    /**
+     * \brief Retrieve the value of a tag pair.
+     *
+     * Returns the value of a tag pair with the given name, if it exists.
+     * Returns nullopt otherwise.
+     * \param name Name of the tag pair.
+     * \return Value of the tag pair, if it exists.
+     */
+    auto get(const std::string &name) const -> std::optional<std::string>;
+
+    /**
+     * \brief Add a tag pair.
+     *
+     * \param name Name of the tag.
+     * \param value Value of the tag.
+     */
+    template<typename NameT, typename ValueT>
+    auto emplace_back(NameT &&name, ValueT &&value) -> void {
+        m_tags.emplace_back(std::forward<NameT>(name), std::forward<ValueT>(value));
+    }
+
+    /**
+     * \brief Determine, if the tag belongs to the seven tag roster (STR).
+     *
+     * The STR tags are: Event, Site, Date, Round, White, Black, Result.
+     * \param name Name of the tag.
+     * \return If the tag belongs to the STR.
+     */
+    static auto is_str_tag(const std::string &name) -> bool;
+
+    /**
+     * \brief Determine, if the tag belongs to the seven tag roster (STR).
+     *
+     * The STR tags are: Event, Site, Date, Round, White, Black, Result.
+     * \param tag The tag.
+     * \return If the tag belongs to the STR.
+     */
+    static auto is_str_tag(const metadata_tag &tag) -> bool;
+private:
+    std::vector<metadata_tag> m_tags{}; ///< Collection of metadata tags.
+};
 
 } // namespace chessgame
 
