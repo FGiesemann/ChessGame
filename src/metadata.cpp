98a9320b822ea52664412d190647ea937629a780
Florian Giesemann <florian.giesemann@gmail.com>
2025-08-28 18:10:11 +0200

    Make GameMetadata its own class

--- src/metadata.cpp
+++ src/metadata.cpp
@@ -4,3 +4,25 @@
  * ************************************************************************** */
 
 #include "chessgame/metadata.h"
+
+namespace chessgame {
+
+const std::array<std::string, 7> GameMetadata::str_tags{"Event", "Site", "Date", "Round", "White", "Black", "Result"};
+
+auto GameMetadata::is_str_tag(const std::string &name) -> bool {
+    return std::ranges::find(str_tags, name) != str_tags.end();
+}
+
+auto GameMetadata::is_str_tag(const metadata_tag &tag) -> bool {
+    return is_str_tag(tag.name);
+}
+
+auto GameMetadata::get(const std::string &name) const -> std::optional<std::string> {
+    const auto tag = std::ranges::find_if(*this, [&name](const metadata_tag &tag) { return tag.name == name; });
+    if (tag == end()) {
+        return std::nullopt;
+    }
+    return tag->value;
+}
+
+} // namespace chessgame
