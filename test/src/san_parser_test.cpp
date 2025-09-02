61e3e436fb1015ea9f5422e6807df53aea76e126
Florian Giesemann <florian.giesemann@gmail.com>
2025-09-02 17:03:04 +0200

    Reorganize SAN tests

--- test/src/san_parser_test.cpp
+++ test/src/san_parser_test.cpp
@@ -55,14 +55,14 @@ auto check_error(const ParseRes &parse_res, SANParserErrorType expected_error) {
     return !parse_res.has_value() && parse_res.error().error_type == expected_error;
 }
 
-TEST_CASE("SAN Parser.Castling", "[san]") {
+TEST_CASE("SAN.Parser.Castling", "[san]") {
     CHECK(check_move(parse_san("O-O", Color::White), Piece::WhiteKing, Square::G1, false, std::nullopt, CheckState::None, std::nullopt));
     CHECK(check_move(parse_san("O-O", Color::Black), Piece::BlackKing, Square::G8, false, std::nullopt, CheckState::None, std::nullopt));
     CHECK(check_move(parse_san("O-O-O", Color::White), Piece::WhiteKing, Square::C1, false, std::nullopt, CheckState::None, std::nullopt));
     CHECK(check_move(parse_san("O-O-O", Color::Black), Piece::BlackKing, Square::C8, false, std::nullopt, CheckState::None, std::nullopt));
 }
 
-TEST_CASE("SAN Parser.Pieces", "[san]") {
+TEST_CASE("SAN.Parser.Pieces", "[san]") {
     CHECK(check_move(parse_san("e4", Color::White), Piece::WhitePawn, Square::E4, false, std::nullopt, CheckState::None, std::nullopt));
     CHECK(check_move(parse_san("c6", Color::Black), Piece::BlackPawn, Square::C6, false, std::nullopt, CheckState::None, std::nullopt));
     CHECK(check_move(parse_san("Nf3", Color::White), Piece::WhiteKnight, Square::F3, false, std::nullopt, CheckState::None, std::nullopt));
@@ -72,12 +72,12 @@ TEST_CASE("SAN Parser.Pieces", "[san]") {
     CHECK(check_move(parse_san("Kg8", Color::White), Piece::WhiteKing, Square::G8, false, std::nullopt, CheckState::None, std::nullopt));
 }
 
-TEST_CASE("SAN Parser.Captures", "[san]") {
+TEST_CASE("SAN.Parser.Captures", "[san]") {
     CHECK(check_move(parse_san("Bxe5", Color::White), Piece::WhiteBishop, Square::E5, true, std::nullopt, CheckState::None, std::nullopt));
     CHECK(check_move(parse_san("Qxd4", Color::Black), Piece::BlackQueen, Square::D4, true, std::nullopt, CheckState::None, std::nullopt));
 }
 
-TEST_CASE("SAN Parser.Disambiguation", "[san]") {
+TEST_CASE("SAN.Parser.Disambiguation", "[san]") {
     CHECK(check_move(parse_san("Ncd4", Color::White), Piece::WhiteKnight, Square::D4, false, std::nullopt, CheckState::None, File{'c'}, std::nullopt));
     CHECK(check_move(parse_san("Ned4", Color::White), Piece::WhiteKnight, Square::D4, false, std::nullopt, CheckState::None, File{'e'}, std::nullopt));
     CHECK(check_move(parse_san("N3d4", Color::White), Piece::WhiteKnight, Square::D4, false, std::nullopt, CheckState::None, Rank{3}, std::nullopt));
@@ -90,18 +90,18 @@ TEST_CASE("SAN Parser.Disambiguation", "[san]") {
     CHECK(check_move(parse_san("Rd1d2", Color::White), Piece::WhiteRook, Square::D2, false, std::nullopt, CheckState::None, File{'d'}, Rank{1}, std::nullopt));
 }
 
-TEST_CASE("SAN Parser.Check", "[san]") {
+TEST_CASE("SAN.Parser.Check", "[san]") {
     CHECK(check_move(parse_san("d7+", Color::White), Piece::WhitePawn, Square::D7, false, std::nullopt, CheckState::Check, std::nullopt));
     CHECK(check_move(parse_san("dxe3#", Color::Black), Piece::BlackPawn, Square::E3, true, std::nullopt, CheckState::Checkmate, std::nullopt));
     CHECK(check_move(parse_san("Qe3+", Color::White), Piece::WhiteQueen, Square::E3, false, std::nullopt, CheckState::Check, std::nullopt));
     CHECK(check_move(parse_san("Qxe3#", Color::Black), Piece::BlackQueen, Square::E3, true, std::nullopt, CheckState::Checkmate, std::nullopt));
 }
 
-TEST_CASE("SAN Parser.Pawn Promotion", "[san]") {
+TEST_CASE("SAN.Parser.Pawn Promotion", "[san]") {
     CHECK(check_move(parse_san("exd8=Q#", Color::White), Piece::WhitePawn, Square::D8, true, Piece::WhiteQueen, CheckState::Checkmate, std::nullopt));
 }
 
-TEST_CASE("SAN Parser.Suffix Annotations", "[san]") {
+TEST_CASE("SAN.Parser.Suffix Annotations", "[san]") {
     CHECK(check_move(parse_san("Qxe2!", Color::White), Piece::WhiteQueen, Square::E2, true, std::nullopt, CheckState::None, SuffixAnnotation::GoodMove));
     CHECK(check_move(parse_san("Bec3!?", Color::Black), Piece::BlackBishop, Square::C3, false, std::nullopt, CheckState::None, SuffixAnnotation::SpeculativeMove));
     CHECK(check_move(parse_san("O-O??", Color::White), Piece::WhiteKing, Square::G1, false, std::nullopt, CheckState::None, SuffixAnnotation::VeryPoorMove));
@@ -110,7 +110,7 @@ TEST_CASE("SAN Parser.Suffix Annotations", "[san]") {
     CHECK(check_move(parse_san("dxe2?!", Color::Black), Piece::BlackPawn, Square::E2, true, std::nullopt, CheckState::None, SuffixAnnotation::QuestionableMove));
 }
 
-TEST_CASE("SAN Parser.Mixed Examples", "[san]") {
+TEST_CASE("SAN.Parser.Mixed Examples", "[san]") {
     CHECK(check_move(parse_san("Bxc3+", Color::Black), Piece::BlackBishop, Square::C3, true, std::nullopt, CheckState::Check, std::nullopt));
     CHECK(check_move(parse_san("dxc5", Color::White), Piece::WhitePawn, Square::C5, true, std::nullopt, CheckState::None, std::nullopt));
     CHECK(check_move(parse_san("Qxd1+", Color::Black), Piece::BlackQueen, Square::D1, true, std::nullopt, CheckState::Check, std::nullopt));
@@ -119,7 +119,7 @@ TEST_CASE("SAN Parser.Mixed Examples", "[san]") {
     CHECK(check_move(parse_san("gxf3+", Color::Black), Piece::BlackPawn, Square::F3, true, std::nullopt, CheckState::Check, std::nullopt));
 }
 
-TEST_CASE("SAN Parser.Invalid SAN", "[san]") {
+TEST_CASE("SAN.Parser.Invalid SAN", "[san]") {
     CHECK(check_error(parse_san("axf9", Color::White), SANParserErrorType::MissingRank));
     CHECK(check_error(parse_san("Lc4", Color::Black), SANParserErrorType::UnexpectedToken));
     CHECK(check_error(parse_san("Kg1a", Color::White), SANParserErrorType::MissingRank));
